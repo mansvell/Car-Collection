244f7e4c53ead79a2c433a13532d32580de2dfb1
mnan63 <mansvell.nkwanga.nkwanga@mni.thm.de>
2025-12-03 21:23:46 +0100

    Weiterleitung

---
+++ CarCollectionBackend/src/main/java/com/example/carcollectionbackend/CarCollectionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.carcollectionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCollectionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarCollectionBackendApplication.class, args);
+    }
+
+}
---
+++ CarCollectionBackend/src/test/java/com/example/carcollectionbackend/CarCollectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carcollectionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCollectionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
