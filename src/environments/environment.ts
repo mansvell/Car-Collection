583347e5426627aa1ed22210c28b20e5b5950c00
mnan63 <mansvell.nkwanga.nkwanga@mni.thm.de>
2026-02-03 20:33:44 +0100

    render connect angular with environmts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'https://carcollection-backend-o8zu.onrender.com/api'
 };
 //afin que angular appelle bien mon Backend.
 // il ya aussi un port dans application.properties pour afficher uniquement le Backend
